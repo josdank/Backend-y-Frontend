92775980c6bfb8b9148282e1ca2d97bb605a61f2
josdank <josue.guerra@epn.edu.ec>
2025-01-23 22:40:56 -0500

    Auth

--- frontend/src/context/AuthProvider.jsx
+++ frontend/src/context/AuthProvider.jsx
@@ -79,7 +79,7 @@ const AuthProvider = ({ children }) => {
             setAuth,
             actualizarPerfil,
             actualizarPassword,
-            setLoad,
+            setLoa,
             loading
         }}>
             {children}
